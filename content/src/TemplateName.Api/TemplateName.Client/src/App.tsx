cfcefd361b2e899762d0c277f0e7b12ac554e605
Jannik Buschke <jbu.extern@silvermonkey.net>
2018-12-22 13:19:20 +0100

    wip

--- content/src/TemplateName.Api/TemplateName.Client/src/App.tsx
+++ content/src/TemplateName.Api/TemplateName.Client/src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './App.css';
 import { Formik } from 'formik';
-import { StringEditor } from '@jannikb/react-glue';
+import { StringEditor, FormikDebug } from '@jannikb/react-glue';
+import { Menu, Icon } from 'antd';
 
 class App extends React.Component {
   public render() {
@@ -11,8 +12,79 @@ class App extends React.Component {
           initialValues={{ firstName: 'petr' }}
           onSubmit={() => {}}
           render={() => (
-            <div>
-              <StringEditor name="firstName" />
+            <div
+              style={{
+                display: 'grid',
+                gridTemplateRows: 'auto 1fr',
+                gridTemplateColumns: '280px 1fr',
+              }}
+            >
+              <Menu
+                mode="horizontal"
+                style={{ gridColumnStart: 1, gridColumnEnd: 3 }}
+                selectedKeys={[]}
+              >
+                <Menu.Item key="mail">
+                  <Icon type="mail" />
+                  Navigation One
+                </Menu.Item>
+                <Menu.Item key="app" disabled>
+                  <Icon type="appstore" />
+                  Navigation Two
+                </Menu.Item>
+                <Menu.SubMenu
+                  selectedKeys={['setting:1']}
+                  title={
+                    <span>
+                      <Icon type="setting" />
+                      Navigation Three - Submenu
+                    </span>
+                  }
+                >
+                  <Menu.ItemGroup title="Item 1" selectedKeys={['setting:1']}>
+                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  </Menu.ItemGroup>
+                  <Menu.ItemGroup title="Item 2">
+                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                  </Menu.ItemGroup>
+                </Menu.SubMenu>
+              </Menu>
+              <Menu>
+                <Menu.Item key="mail">
+                  <Icon type="mail" />
+                  Navigation One
+                </Menu.Item>
+                <Menu.Item key="app" disabled>
+                  <Icon type="appstore" />
+                  Navigation Two
+                </Menu.Item>
+                <Menu.SubMenu
+                  title={
+                    <span className="submenu-title-wrapper">
+                      <Icon type="setting" />
+                      Navigation Three - Submenu
+                    </span>
+                  }
+                >
+                  <Menu.ItemGroup title="Item 1">
+                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  </Menu.ItemGroup>
+                  <Menu.ItemGroup title="Item 2">
+                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                  </Menu.ItemGroup>
+                </Menu.SubMenu>
+              </Menu>
+
+              <div style={{ margin: 20 }}>
+                <StringEditor name="firstName" />
+                <pre>
+                  <FormikDebug />
+                </pre>
+              </div>
             </div>
           )}
         />
--- content/src/TemplateName.Api/TemplateName.Client/src/index.tsx
+++ content/src/TemplateName.Api/TemplateName.Client/src/index.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
