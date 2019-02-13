7a7fc0c25581f93e5a36ce018fb213a1fa0cb96d
Jannik Buschke <jbu.extern@silvermonkey.net>
2019-02-13 12:59:11 +0100

    update Routes, css, and tsconfig

--- content/src/TemplateName.Api/TemplateName.Client/src/App.tsx
+++ content/src/TemplateName.Api/TemplateName.Client/src/App.tsx
@@ -1,83 +1,86 @@
 import * as React from 'react';
 import './App.css';
-import { Menu, Icon } from 'antd';
+import { Icon, Badge } from 'antd';
 import { SampleForm } from './SampleForm';
-import { Link, Router } from '@reach/router';
+import { Router } from '@reach/router';
 import { CreateContact } from './Contacts/CreateContact';
+import {
+  ApplicationLayout,
+  HorizontalSplit,
+  MasterDetailView,
+} from '@jbuschke/react-glue';
+import { ContactList } from './Contacts/ContactList';
+import { EditContact } from './Contacts/EditContact';
+import { Home } from './Home/Home';
+
+const HelloWorld = (props: any) => <h1>Hello world</h1>;
 
 class App extends React.Component {
   public render() {
     return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateRows: 'auto 1fr',
-          gridTemplateColumns: '280px 1fr',
-        }}
+      <ApplicationLayout
+        header={[{ displayName: 'Home', to: '/', kind: 'LINK', icon: 'home' }]}
+        sideBarItems={[
+          {
+            kind: 'LINK',
+            displayName: 'Formik-Antd',
+            to: '/formik-antd',
+            icon: 'form',
+          },
+          {
+            kind: 'LINK',
+            displayName: 'Contacts',
+            to: '/contacts',
+            icon: 'user',
+          },
+        ]}
+        headerRight={[
+          {
+            kind: 'CUSTOM',
+            component: () => (
+              <div style={{ marginRight: 15, display: 'inline' }}>
+                <Badge dot={true}>
+                  <Icon type="bell" />
+                </Badge>
+              </div>
+            ),
+          },
+          {
+            kind: 'CUSTOM',
+            component: () => (
+              <div style={{ marginRight: 15, display: 'inline' }}>
+                <Icon type="user" />
+              </div>
+            ),
+          },
+          {
+            kind: 'CUSTOM',
+            component: () => <Icon type="setting" />,
+          },
+        ]}
       >
-        <Menu
-          mode="horizontal"
-          style={{ gridColumnStart: 1, gridColumnEnd: 3 }}
-          selectedKeys={[]}
-        >
-          <Menu.Item key="mail">
-            <Icon type="mail" />
-            Navigation One
-          </Menu.Item>
-          <Menu.Item key="app" disabled>
-            <Icon type="appstore" />
-            Navigation Two
-          </Menu.Item>
-          <Menu.SubMenu
-            title={
-              <span>
-                <Icon type="setting" />
-                Navigation Three - Submenu
-              </span>
-            }
-          >
-            <Menu.ItemGroup title="Item 1" selectedKeys={['setting:1']}>
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </Menu.ItemGroup>
-          </Menu.SubMenu>
-        </Menu>
-        <Menu>
-          <Menu.Item key="user">
-            <Link to="contacts">
-              <Icon type="user" />
-              Contacts
-            </Link>
-          </Menu.Item>
-
-          <Menu.SubMenu
-            title={
-              <span className="submenu-title-wrapper">
-                <Icon type="setting" />
-                Navigation Three - Submenu
-              </span>
-            }
-          >
-            <Menu.ItemGroup title="Item 1">
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </Menu.ItemGroup>
-          </Menu.SubMenu>
-        </Menu>
-        <Router>
-          <SampleForm path="/" />
-          <CreateContact path="/contacts" />
-        </Router>
-        )}
-      </div>
+        <>
+          <Router primary={false}>
+            <Home path="/" />
+          </Router>
+          <HorizontalSplit templateColumns="1fr 1fr">
+            <MasterDetailView
+              item={{
+                detail: EditContact,
+                list: ContactList,
+                new: CreateContact,
+                path: '/contacts',
+              }}
+            />
+          </HorizontalSplit>
+          <Router primary={false}>
+            <HelloWorld path="/hello-world" />
+          </Router>
+          <Router primary={false}>
+            <SampleForm path="/formik-antd" />
+          </Router>
+        </>
+      </ApplicationLayout>
     );
   }
 }
